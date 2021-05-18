12a3c6f368054a1451ce3306eda4324a72d78019
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2021-05-18 14:51:09 +0530

    initial test checkup
    Signed-off-by: Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
-import { useAuthState } from "../context/auth";
+import { useAuthState ,useAuthDispatch} from "../context/auth";
 
 export default function IndexPage(){
-    const {isAuthenticated} = useAuthState();
+    const {isAuthenticated,user} = useAuthState();
+    const {login,createUser,logout}= useAuthDispatch()
+
     return(
-    <> {isAuthenticated ?"Hello User":"Hello Guest"}</>)
+    <> 
+    {isAuthenticated ?(
+        <>
+    <p>Hello {user.name}</p>
+    <button onClick={logout}>logout</button>
+    </>
+    ):(<button onClick={()=>
+        login({
+            email:"j@j.com",
+            password:"abc123"
+        })}>
+            Login
+    </button>)}
+    
+    </>)
 }
